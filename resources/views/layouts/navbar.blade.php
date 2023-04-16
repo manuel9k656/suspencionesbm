bb81465c17b578f26a10d8b79070b2b45b8f907d
manuel9k656 <manuelmtz9k@gmail.com>
2023-04-16 10:04:23 -0600

    logo

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -10,16 +10,9 @@
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <div class="dropdown">
-            <a class=" dropdown-toggle text-white" type="button" data-toggle="dropdown" aria-expanded="false">
-               {{-- {{ Auth::user()->name }} --}}
-               Log Out
-            </a>
+            <a class=" dropdown-toggle text-white" type="button" data-toggle="dropdown" aria-expanded="false">{{ Auth::user()->name }}</a>
             <div class="dropdown-menu">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                              document.getElementById('logout-form').submit();">
-                 {{ __('Logout') }}
-             </a>
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
 
              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                  @csrf
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,8 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a  class="brand-link bg-primary">
-      <img  src="{{ asset('dist/img/llave.png') }}" alt="Daylight" class=" brand-image img-circle elevation-3" style="opacity: .8">
+    <a  class="brand-link bg-white">
+      <img  src="{{ asset('dist/img/logo-1.png') }}" alt="SBM" class=" brand-image img-circle elevation-3" style="opacity: .8;">
       <span class="brand-text font-weight-light">Suspenciones BM</span>
     </a>
 
@@ -20,9 +20,7 @@
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-header">Tienda</li>
+          {{-- <li class="nav-header">Tienda</li>
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-folder"></i>
@@ -33,7 +31,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                {{-- <a href="{{route('workorder/index')}}" class="nav-link"> --}}
+                <a href="{{route('workorder/index')}}" class="nav-link">
                   <i class="text-success far fa-circle nav-icon"></i>
                   <p>Abiertas</p>
                 </a>
@@ -41,44 +39,35 @@
             </ul>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                {{-- <a href="{{route('workorder/order_close')}}" class="nav-link"> --}}
+                <a href="{{route('workorder/order_close')}}" class="nav-link">
                   <i class=" text-danger far fa-circle nav-icon"></i>
                   <p>Cerradas</p>
                 </a>
               </li>
             </ul>
-          </li>
+          </li> --}}
 
-          <li class="nav-header">ALMACEN</li>
+          {{-- <li class="nav-header">Tienda</li> --}}
           <li class="nav-item">
-            {{-- <a href="{{route('shipping/shipping_active')}}" class="nav-link"> --}}
-              <i class="nav-icon fas fa-truck"></i>
-              <p class="text">Envios</p>
-            </a>
-          </li>
-
-
-          <li class="nav-header">FACTURACION</li>
-          <li class="nav-item">
-            {{-- <a href="{{route('invoice/index')}}" class="nav-link"> --}}
+            <a href="{{route('home')}}" class="nav-link">
               <i class=" nav-icon fas fa-file-invoice-dollar"></i>
-              <p class="text">Facturas</p>
+              <p class="text">Punto de venta</p>
             </a>
           </li>
 
-@php
-    $id = Auth::id();
-@endphp
-        @if ($id ==1 )
+        @php
+          $id = Auth::id();
+        @endphp
+        @if ($id == 1 )
           <li class="nav-header">Administracion</li>
           <li class="nav-item">
-            {{-- <a href="{{route('admin/users')}}" class="nav-link"> --}}
+            <a href="{{route('home')}}" class="nav-link">
               <i class=" nav-icon fas fa-users "></i>
               <p class="text">Usuarios</p>
             </a>
           </li>
           <li class="nav-item">
-            {{-- <a href="{{route('register')}}" class="nav-link"> --}}
+            <a href="{{route('home')}}" class="nav-link">
               <i class=" nav-icon fas fa-users "></i>
               <p class="text">registrar</p>
             </a>
